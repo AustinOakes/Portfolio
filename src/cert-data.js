f7743a736b4c54985603b3c3273e1ad689013139
Austin Oakes <aoakes208@gmail.com>
2024-07-16 19:45:59 -0400

    Added quotes around certification descriptions

--- src/cert-data.js
+++ src/cert-data.js
@@ -1,6 +1,6 @@
 export let pbi_details = {
     title: "Microsoft Certified: Power BI Data Analyst Associate",
-    description: "Demonstrate methods and best practices that align with business and technical requirements for modeling, visualizing, and analyzing data with Microsoft Power BI.",
+    description: '"Demonstrate methods and best practices that align with business and technical requirements for modeling, visualizing, and analyzing data with Microsoft Power BI."',
     earnedDate: "May 11, 2023",
     expirationDate: "May 11, 2025",
     image: "power-bi",
@@ -8,7 +8,7 @@ export let pbi_details = {
 
 export let az_details = {
     title: "Microsoft Certified: Azure Fundamentals",
-    description: "Demonstrate foundational knowledge of cloud concepts, core Azure services, plus Azure management and governance features and tools.",
+    description: '"Demonstrate foundational knowledge of cloud concepts, core Azure services, plus Azure management and governance features and tools."',
     earnedDate: "March 19, 2022",
     expirationDate: "N/A",
     image: "azure",
@@ -16,7 +16,7 @@ export let az_details = {
 
 export let powplat_details = {
     title: "Microsoft Certified: Power Platform Fundamentals",
-    description: "Demonstrate the business value and product capabilities of Microsoft Power Platform, such as Power Apps, data connections with Dataverse, and Power Automate.",
+    description: '"Demonstrate the business value and product capabilities of Microsoft Power Platform, such as Power Apps, data connections with Dataverse, and Power Automate."',
     earnedDate: "October 30, 2022",
     expirationDate: "N/A",
     image: "power-platform",
